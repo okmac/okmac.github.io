da4fd8bf6dd401c8778dffa2e7b2f7a5babd25a0
okmac <8130803+okmac@users.noreply.github.com>
2019-03-16 23:30:30 -0600

    Delete NoteCard.kt

--- app/src/main/java/blue/roygbiv/nafap/NoteCard.kt
+++
@@ -1,7 +0,0 @@
-package blue.roygbiv.nafap
-
-class NoteCard(name: String, description: String, category: Category) {
-    val name = name
-    val description = description
-    val category = category
-}
